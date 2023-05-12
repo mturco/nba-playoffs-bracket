d5c54fe9dda0ed18db06dacfc6a3ec912b8ed055
Matt Turco <matt.turco@gmail.com>
2023-05-12 09:30:46 -0600

    Upcoming games display tweaks

--- src/lib/Games.svelte
+++ src/lib/Games.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { format, parseISO } from 'date-fns';
+	import { format, isToday, parseISO } from 'date-fns';
 	import type { Game } from '../types';
 
 	export let games: Map<string, Game[]>;
@@ -9,14 +9,16 @@
 	<h1 class="text-3xl mb-8 text-center">Upcoming Games</h1>
 
 	{#each Array.from(games.entries()) as [date, gamesForDate]}
-		<h2 class="text-gray-600 dark:text-gray-400">{format(parseISO(date), 'EEEE, MMMM d')}</h2>
+		<h2 class="text-gray-600 dark:text-gray-400">
+			{isToday(parseISO(date)) ? 'Today' : format(parseISO(date), 'EEEE, MMMM d')}
+		</h2>
 
 		<ol class="mb-8">
 			{#each gamesForDate as game}
 				<li class="my-2 p-4 rounded-md shadow-md bg-gray-200 dark:bg-gray-700">
 					<p>
-						<span class="font-medium">{game.highSeedTricode}</span> vs
-						<span class="font-medium">{game.lowSeedTricode}</span>
+						<span class="font-medium">{game.highSeedTricode || '???'}</span> vs
+						<span class="font-medium">{game.lowSeedTricode || '???'}</span>
 						— {game.number}
 					</p>
 					<p class="text-gray-600 dark:text-gray-400">
